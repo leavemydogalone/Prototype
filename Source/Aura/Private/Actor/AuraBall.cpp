d0a0287a82d0e3a37db4f386dbdfab83507c745f
leavemydogalone <niklasport88@gmail.com>
2025-04-22 17:20:25 -0700

    added asc to ball

--- Source/Aura/Private/Actor/AuraBall.cpp
+++ Source/Aura/Private/Actor/AuraBall.cpp
@@ -3,14 +3,31 @@
 
 #include "Actor/AuraBall.h"
 #include "Game/TurnSystemInterface.h"
+#include "AbilitySystemComponent.h"
+#include "AuraGameplayTags.h"
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
 #include "Game/AuraTurnPhase.h"
 
 AAuraBall::AAuraBall()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Minimal);
+
+	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+
+	const FAuraGameplayTags& GameplayTags = FAuraGameplayTags::Get();
 
 	BallMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BallMesh"));
 	BallMesh->SetupAttachment(RootComponent);
+}
+
+UAbilitySystemComponent* AAuraBall::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
 }
 
 void AAuraBall::BeginPlay()
@@ -22,6 +39,9 @@ void AAuraBall::BeginPlay()
 		FOnTurnPhaseChange& OnTurnPhaseChange = TurnSystemInterface->GetOnTurnPhaseChangeDelegate();
 		OnTurnPhaseChange.AddUObject(this, &AAuraBall::HandleTurnPhaseChange);
 	}
+
+	InitAbilityActorInfo();
+
 }
 
 void AAuraBall::HandleTurnPhaseChange(EAuraTurnPhase NewTurnPhase)
@@ -60,3 +80,40 @@ void AAuraBall::UnFreezeBall()
 }
 
 
+void AAuraBall::ApplyEffectToSelf(TSubclassOf<UGameplayEffect> GameplayEffectClass, float Level) const
+{
+	check(IsValid(GetAbilitySystemComponent()));
+	check(GameplayEffectClass);
+	FGameplayEffectContextHandle ContextHandle = GetAbilitySystemComponent()->MakeEffectContext();
+	ContextHandle.AddSourceObject(this);
+	const FGameplayEffectSpecHandle SpecHandle = GetAbilitySystemComponent()->MakeOutgoingSpec(GameplayEffectClass, Level, ContextHandle);
+	GetAbilitySystemComponent()->ApplyGameplayEffectSpecToTarget(*SpecHandle.Data.Get(), GetAbilitySystemComponent());
+}
+
+void AAuraBall::InitAbilityActorInfo()
+{
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
+	Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)->AbilityActorInfoSet();
+
+	if (HasAuthority())
+	{
+		//InitializeDefaultAttributes();
+		AddCharacterAbilities();
+	}
+	//~ Can add the below if I want to attach the niagara and debuff components
+	//OnAscRegistered.Broadcast(AbilitySystemComponent);
+}
+
+void AAuraBall::AddCharacterAbilities()
+{
+	UAuraAbilitySystemComponent* AuraASC = CastChecked<UAuraAbilitySystemComponent>(AbilitySystemComponent);
+	if (!HasAuthority()) return;
+
+	AuraASC->AddCharacterAbilities(StartupAbilities);
+	AuraASC->AddCharacterPassiveAbilities(StartupPassiveAbilities);
+}
+
+void AAuraBall::InitializeDefaultAttributes() const
+{
+	ApplyEffectToSelf(DefaultBallAttributes, 1.f);
+}
--- Source/Aura/Public/Actor/AuraBall.h
+++ Source/Aura/Public/Actor/AuraBall.h
@@ -5,16 +5,24 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "Game/AuraTurnPhase.h"
+#include "AbilitySystemInterface.h"
 #include "AuraBall.generated.h"
 
+class UAbilitySystemComponent;
+class UAttributeSet;
+class UGameplayAbility;
+class UGameplayEffect;
 
 UCLASS()
-class AURA_API AAuraBall : public AActor
+class AURA_API AAuraBall : public AActor, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 	
 public:	
 	AAuraBall();
+
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
 
 protected:
 	virtual void BeginPlay() override;
@@ -24,6 +32,23 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ball Components")
 	TObjectPtr<UStaticMeshComponent> BallMesh;
 
+	UPROPERTY()
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY()
+	TObjectPtr<UAttributeSet> AttributeSet;
+
+	void ApplyEffectToSelf(TSubclassOf<UGameplayEffect> GameplayEffectClass, float Level) const;
+
+	virtual void InitAbilityActorInfo();
+
+	void AddCharacterAbilities();
+
+	virtual void InitializeDefaultAttributes() const;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Attributes")
+	TSubclassOf<UGameplayEffect> DefaultBallAttributes;
+
 private:
 
     FVector StoredLinearVelocity;  
@@ -31,4 +56,11 @@ private:
 
 	void FreezeBall();
 	void UnFreezeBall();
+
+	UPROPERTY(EditAnywhere, Category = "Abilities")
+	TArray<TSubclassOf<UGameplayAbility>> StartupAbilities;
+
+	UPROPERTY(EditAnywhere, Category = "Abilities")
+	TArray<TSubclassOf<UGameplayAbility>> StartupPassiveAbilities;
+
 };
